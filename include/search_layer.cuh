94e08c858622c964106c35250c14955fca5b3211
joshuacappelli <joshua@cappelliconnect.com>
2025-01-02 16:13:05 -0500

    working bloomfilter

--- include/search_layer.cuh
+++ include/search_layer.cuh
@@ -6,6 +6,7 @@
 #include "euclidean_distance.cuh"
 #include "data_structures.cuh"
 #include "utils.cuh"
+#include "bloom_filter.cuh"
 
 using namespace ds;
 
@@ -17,7 +18,7 @@ template <typename T = float>
 __global__ void search_layer_kernel(
     T* queries,
     int* start_ids,
-    int* adjaecy_list,
+    int* adjacency_list,
     T* dataset,
     int* ef,
     d_Neighbor<T>* result
@@ -28,12 +29,17 @@ __global__ void search_layer_kernel(
     T* query = queries + bidx * VEC_DIM;
     int start_node_id = start_ids[bidx];
 
-
     // Shared memory for query data
-    static __shared__ T shared_query[VEC_DIM];
+    __shared__ T shared_query[VEC_DIM];
+    __shared__ SharedBloomFilter bloom_filter;
+
     if (tidx < VEC_DIM) {
         shared_query[tidx] = query[tidx];
     }
+    if (tidx == 0) {
+        bloom_filter.init();
+    }
+    __syncthreads();
 
     // Priority queues initialization
     __shared__ d_Neighbor<T> candidates_array[MAX_HEAP_SIZE];
@@ -46,7 +52,7 @@ __global__ void search_layer_kernel(
     top_candidates_size = 0;
     PriorityQueue<T> topk(top_candidates_array, &top_candidates_size, MAX_HEAP);
 
-    // calculate distance from start node to query and add to queue
+    // Calculate distance from start node to query and add to queue
     T start_dist = euclidean_distance_gpu<T>(
         shared_query,
         dataset + start_node_id * VEC_DIM,
@@ -56,14 +62,9 @@ __global__ void search_layer_kernel(
     if (tidx == 0) {
         q.insert({start_dist, start_node_id});
         topk.insert({start_dist, start_node_id});
+        bloom_filter.set(start_node_id); // Mark the start node as visited
     }
     __syncthreads();
-
-    // Shared memory for visited nodes
-    bool shared_visited[DATASET_SIZE];
-    if (tidx == 0) {
-        shared_visited[start_node_id] = true;
-    }
 
     // Main loop
     while (q.get_size() > 0) {
@@ -71,31 +72,22 @@ __global__ void search_layer_kernel(
 
         // Get the current node information
         d_Neighbor<T> now = q.pop();
-        int* neighbors_ids = adjaecy_list + now.id * K;
-
-        if (tidx == 0) {
-            // Mark current node as visited
-            shared_visited[now.id] = true;
-            // printf("Visiting node: %d\n", now.id);
-        }
+        int* neighbors_ids = adjacency_list + now.id * K;
 
         int n_neighbors = 0;
         while (neighbors_ids[n_neighbors] != -1 && n_neighbors < K) {
-            // count number of neighbors in current node
             n_neighbors++;
         }
 
-        
-
-        // Check the exit condidtion
-        bool c = topk.top().dist < now.dist;
-        if (c) {
+        // Check the exit condition
+        if (topk.top().dist < now.dist) {
             break;
         }
         __syncthreads();
 
-        // distance computation (convert to bulk?)
+        // Shared memory for distances
         static __shared__ T shared_distances[K];
+
         for (size_t i = 0; i < n_neighbors; i++) {
             T dist = euclidean_distance_gpu<T>(
                 shared_query,
@@ -108,32 +100,25 @@ __global__ void search_layer_kernel(
             }
         }
         __syncthreads();
-        
+
         // Update priority queues
         if (tidx == 0) {
             for (size_t i = 0; i < n_neighbors; i++) {
-                if (shared_visited[neighbors_ids[i]]) continue;
-                shared_visited[neighbors_ids[i]] = true;
+                int neighbor_id = neighbors_ids[i];
+                if (bloom_filter.test(neighbor_id)) continue; // Check if visited
+                bloom_filter.set(neighbor_id); // Mark as visited
 
-                if (shared_distances[i < topk.top().dist] ||
+                if (shared_distances[i] < topk.top().dist ||
                     topk.get_size() < *ef) {
                     
-                    q.insert({shared_distances[i], neighbors_ids[i]});
-                    topk.insert({shared_distances[i], neighbors_ids[i]});
+                    q.insert({shared_distances[i], neighbor_id});
+                    topk.insert({shared_distances[i], neighbor_id});
 
                     if (topk.get_size() > *ef) {
                         topk.pop();
                     }
-
                 }
             }
-            // printf("Queue size: %d\n", q.get_size());
-            // q.print_heap();
-            // printf("\n");
-            // printf("Topk size: %d\n", topk.get_size());
-            // topk.print_heap();
-            // printf("=====================================\n");
-            // printf("\n");
         }
         __syncthreads();
     }
@@ -147,8 +132,8 @@ __global__ void search_layer_kernel(
             result[blockIdx.x * (*ef) + i] = topk.pop();
         }
     }
-
 }
+
 
 template <typename T = float>
 SearchResults search_layer_launch(
