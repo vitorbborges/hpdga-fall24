dc33045c5f5677f12f6e5effe7b86b4a997c2978
vitorbborges <vitorbborges31@gmail.com>
2025-01-03 01:47:57 +0100

    Merge optimal knn-search kernel into development branch

--- include/search_layer.cuh
+++ include/search_layer.cuh
@@ -1,4 +1,3 @@
-
 #ifndef HNSW_SEARCH_LAYER_CUH
 #define HNSW_SEARCH_LAYER_CUH
 
@@ -7,6 +6,8 @@
 #include "euclidean_distance.cuh"
 #include "data_structures.cuh"
 #include "utils.cuh"
+#include "bloom_filter.cuh"
+
 
 using namespace ds;
 
@@ -17,8 +18,8 @@ using namespace ds;
 template <typename T = float>
 __global__ void search_layer_kernel(
     T* queries,
-    int* start_node_id,
-    int* adjaecy_list,
+    d_Neighbor<T>* start_ids,
+    int* adjacency_list,
     T* dataset,
     int* ef,
     d_Neighbor<T>* result
@@ -27,11 +28,17 @@ __global__ void search_layer_kernel(
     int bidx = blockIdx.x;
 
     T* query = queries + bidx * VEC_DIM;
+    int start_node_id = start_ids[bidx].id;
 
     // Shared memory for query data
-    static __shared__ T shared_query[VEC_DIM];
+    __shared__ T shared_query[VEC_DIM];
+    __shared__ SharedBloomFilter bloom_filter;
+
     if (tidx < VEC_DIM) {
         shared_query[tidx] = query[tidx];
+    }
+    if (tidx == 0) {
+        bloom_filter.init();
     }
     __syncthreads();
 
@@ -46,24 +53,19 @@ __global__ void search_layer_kernel(
     top_candidates_size = 0;
     PriorityQueue<T> topk(top_candidates_array, &top_candidates_size, MAX_HEAP);
 
-    // calculate distance from start node to query and add to queue
+    // Calculate distance from start node to query and add to queue
     T start_dist = euclidean_distance_gpu<T>(
         shared_query,
-        dataset + (*start_node_id) * VEC_DIM,
+        dataset + start_node_id * VEC_DIM,
         VEC_DIM
     );
-    __syncthreads();
 
     if (tidx == 0) {
-        q.insert({start_dist, *start_node_id});
+        q.insert({start_dist, start_node_id});
+        topk.insert({start_dist, start_node_id});
+        bloom_filter.set(start_node_id); // Mark the start node as visited
     }
     __syncthreads();
-
-    // Shared memory for visited nodes
-    bool shared_visited[DATASET_SIZE];
-    if (tidx == 0) {
-        shared_visited[*start_node_id] = true;
-    }
 
     // Main loop
     while (q.get_size() > 0) {
@@ -71,40 +73,26 @@ __global__ void search_layer_kernel(
 
         // Get the current node information
         d_Neighbor<T> now = q.pop();
-        int* neighbors_ids = adjaecy_list + now.id * K;
-
-        if (tidx == 0) {
-            // Mark current node as visited
-            shared_visited[now.id] = true;
-        }
+        int* neighbors_ids = adjacency_list + now.id * K;
 
         int n_neighbors = 0;
         while (neighbors_ids[n_neighbors] != -1 && n_neighbors < K) {
-            // count number of neighbors in current node
             n_neighbors++;
         }
 
-        // Copy neighbor data to shared memory (test if this is faster)
-        static __shared__ T shared_neighbor_data[K * VEC_DIM];
-        for (size_t i = 0; i < n_neighbors; i++) {
-            shared_neighbor_data[i * VEC_DIM + tidx] = dataset[neighbors_ids[i] * VEC_DIM + tidx];
-        }
-        __syncthreads();
-
-        // Update topk
-        if (topk.get_size() == *ef && topk.top().dist < now.dist) {
+        // Check the exit condition
+        if (topk.top().dist < now.dist) {
             break;
-        } else if (tidx == 0) {
-            topk.insert(now);
         }
         __syncthreads();
 
-        // distance computation (convert to bulk?)
+        // Shared memory for distances
         static __shared__ T shared_distances[K];
+
         for (size_t i = 0; i < n_neighbors; i++) {
             T dist = euclidean_distance_gpu<T>(
                 shared_query,
-                shared_neighbor_data + i * VEC_DIM,
+                dataset + neighbors_ids[i] * VEC_DIM,
                 VEC_DIM
             );
             __syncthreads();
@@ -113,13 +101,23 @@ __global__ void search_layer_kernel(
             }
         }
         __syncthreads();
-        
+
         // Update priority queues
         if (tidx == 0) {
             for (size_t i = 0; i < n_neighbors; i++) {
-                if (!shared_visited[neighbors_ids[i]]) {
-                    shared_visited[neighbors_ids[i]] = true;
-                    q.insert({shared_distances[i], neighbors_ids[i]});
+                int neighbor_id = neighbors_ids[i];
+                if (bloom_filter.test(neighbor_id)) continue; // Check if visited
+                bloom_filter.set(neighbor_id); // Mark as visited
+
+                if (shared_distances[i] < topk.top().dist ||
+                    topk.get_size() < *ef) {
+                    
+                    q.insert({shared_distances[i], neighbor_id});
+                    topk.insert({shared_distances[i], neighbor_id});
+
+                    if (topk.get_size() > *ef) {
+                        topk.pop();
+                    }
                 }
             }
         }
@@ -135,95 +133,6 @@ __global__ void search_layer_kernel(
             result[blockIdx.x * (*ef) + i] = topk.pop();
         }
     }
-}
-
-template <typename T = float>
-SearchResults search_layer_launch(
-    const Dataset<T>& queries,
-    const int& start_node_id,
-    const int& ef,
-    const std::vector<Layer>& layers,
-    const int& l_c,
-    const size_t& ds_size,
-    const Dataset<T>& dataset
-) {
-    // Allocate query on device
-    T* d_queries;
-    cudaMalloc(&d_queries, queries.size() * VEC_DIM * sizeof(T));
-    for (size_t i = 0; i < queries.size(); i++) {
-        cudaMemcpy(d_queries + i * VEC_DIM, queries[i].data(), VEC_DIM * sizeof(T), cudaMemcpyHostToDevice);
-    }
-
-    // Allocate start node id on device
-    int* d_start_node_id;
-    cudaMalloc(&d_start_node_id, sizeof(int));
-    cudaMemcpy(d_start_node_id, &start_node_id, sizeof(int), cudaMemcpyHostToDevice);
-
-    // Allocate fixed degree graph adjency list on device
-    int* d_adjaency_list;
-    cudaMalloc(&d_adjaency_list, ds_size * K * sizeof(int));
-    for (Node node: layers[l_c]) {
-        int neighbors_ids[K];
-        std::fill(neighbors_ids, neighbors_ids + K, -1);
-        int node_neighbors_size = node.neighbors.size();
-        if (node_neighbors_size > 0) {
-            for (size_t i = 0; i < node_neighbors_size; i++) {
-                neighbors_ids[i] = node.neighbors[i].id;
-            }
-            cudaMemcpy(d_adjaency_list + node.data.id() * K, neighbors_ids, K * sizeof(int), cudaMemcpyHostToDevice);
-        }
-    }    
-
-    // Allocate dataset on device
-    T* d_dataset;
-    cudaMalloc(&d_dataset, ds_size * VEC_DIM * sizeof(T));
-    for (size_t i = 0; i < ds_size; i++) {
-        cudaMemcpy(d_dataset + i * VEC_DIM, dataset[i].data(), VEC_DIM * sizeof(T), cudaMemcpyHostToDevice);
-    }
-
-    // Allocate Target output lenght
-    int* d_ef;
-    cudaMalloc(&d_ef, sizeof(int));
-    cudaMemcpy(d_ef, &ef, sizeof(int), cudaMemcpyHostToDevice);
-
-    // Allocate result on device
-    d_Neighbor<T> result[ef * queries.size()];
-    d_Neighbor<T>* d_result;
-    cudaMalloc(&d_result, ef * queries.size() * sizeof(d_Neighbor<T>));
-
-    // Launch kernel
-    search_layer_kernel<<<queries.size(), VEC_DIM>>>(
-        d_queries,
-        d_start_node_id,
-        d_adjaency_list,
-        d_dataset,
-        d_ef,
-        d_result
-    );
-
-    // copy result back to host
-    cudaMemcpy(result, d_result, ef * queries.size() * sizeof(d_Neighbor<T>), cudaMemcpyDeviceToHost);
-
-    // Free memory
-    cudaFree(d_start_node_id);
-    cudaFree(d_queries);
-    cudaFree(d_adjaency_list);
-    cudaFree(d_dataset);
-    cudaFree(d_ef);
-    cudaFree(d_result);
-
-    // Prepare output
-    SearchResults search_results(queries.size());
-    for (size_t i = 0; i < queries.size(); i++) {
-        SearchResult search_result = SearchResult();
-        for (size_t j = 0; j < ef; j++) {
-            search_result.result.emplace_back(result[i * ef + j].dist, result[i * ef + j].id);
-        }
-        std::sort(search_result.result.begin(), search_result.result.end(), CompLess());
-        search_results.push_back(search_result);
-    }
-
-    return search_results;
 }
 
 #endif // HNSW_SEARCH_LAYER_CUH
